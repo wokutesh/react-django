73eed6265681231e6e4b03cf85123c8c93488d4d
wokuma <wokumateshome13@gmail.com>
2024-08-22 09:43:35 +0300

    commit

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,3 +2,26 @@ import React,{useEffect} from 'react'
 import {useDispatch,useSelector} from 'react-redux'
 import {fetchCategories} from '../redux/action/categoriesActions'
 import {fetchProducts} from '../redux/action/productsActions'
+
+import CategoryLsit from '../components/CategoryList'
+import FlashSaleProducts from '../components/FlashSaleProducts'
+import TopSellingProducts from '../components/TopSellingProducts'
+import CategoryProducts from '../components/CategoryProducts'
+
+const HomePage=()=>{
+   const dispatch= useDispatch();
+   const categories = useSelector((state)=> state.categories)
+   const products = useSelector((state)=> state.products)
+   
+   useEffect(()=>{
+    dispatch(fetchCategories())
+    dispatch(fetchProducts())
+   },[dispatch])
+}
+
+return (
+
+    <>
+    
+    </>
+)
