18b468cca30cefd30a6b3e692ee6a8bb350cc2cf
wokuma <wokumateshome13@gmail.com>
2024-08-23 18:23:33 +0300

    commit

--- frontend/src/components/TopSellingProducts.js
+++ frontend/src/components/TopSellingProducts.js
@@ -11,7 +11,7 @@ const TopSellingProducts = ({ products }) => {
       <h3>Top Selling Products</h3>
       <div className="row">
         {topSellingProducts.map((product) => (
-          <div key={product.id} className="col-md-3">
+          <div key={product.id} className="col-md-4">
             <Link to={`/products/${product.slug}`}>
               <img src={product.image} alt={product.name} />
               <h4>{product.name}</h4>
