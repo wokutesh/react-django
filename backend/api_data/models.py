c4d6e11487375ead3d7fb60f1fc64793ee4b9711
wokuma <wokumateshome13@gmail.com>
2024-08-23 18:22:20 +0300

    commit

--- backend/api_data/models.py
+++ backend/api_data/models.py
@@ -38,6 +38,7 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=10,decimal_places=2)
     stock=models.PositiveIntegerField()
     image=models.ImageField(upload_to='products')
+    is_flash_sale=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
