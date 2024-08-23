7431c697be0cd422781227292685bea9fe7f37c2
wokuma <wokumateshome13@gmail.com>
2024-08-23 16:32:35 +0300

    commit

--- frontend/src/redux/action/productsActions.js
+++ frontend/src/redux/action/productsActions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const fetchProducts=()=>async(dispatch)=>{
 
     try{
-        const response= await axios.get('/api/products/')
+        const response= await axios.get('http://127.0.0.1:8000/api/products/')
         dispatch({
             type: 'SET_PRODUCTS',
             payload:response.data,
