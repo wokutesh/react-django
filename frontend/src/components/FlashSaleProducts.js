61c442d844945edaf4fe3cb2cd0fef5c93405d7b
wokuma <wokumateshome13@gmail.com>
2024-08-23 18:23:02 +0300

    commit

--- frontend/src/components/FlashSaleProducts.js
+++ frontend/src/components/FlashSaleProducts.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
+import {useNavigate} from 'react-router-dom'
+
 
 const FlashSaleProducts=({products})=>{
-   const flashSaleProducts=products.filter((product)=>product.isFlashSale)
-   return(
-    <div>
-        <h3>Flash Sales</h3>
-    </div>
-   )
+    const navigate=useNavigate();
+    const [currentIndex,setCurrentIndex]=useState(0)
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCurrentIndex(prevIndex =>(prevIndex + 1)% products.length);
+
+        },3000)
+
+        return ()=> clearInterval(interval);
+    },[products.length])
+   
+    const handleClick=()=>{
+        const product =products[currentIndex]
+        navigate('/products/${product.id}');
+
+        return(
+            <div className="flash-sale-slider" onClick={handleClick}>
+                {products.map((product,index)=>{
+                    <img key={index}
+                    src={product.image}
+                    alt={product.name}
+                    className={index===currentIndex ? "active" :""}
+                    />
+                })}
+
+            </div>
+        )
+    }
 }
 
 export default FlashSaleProducts;
