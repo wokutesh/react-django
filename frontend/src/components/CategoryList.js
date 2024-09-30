0f05fb70926f386e5bcc9688f7c73b3022cd1d6a
wokuma <wokumateshome13@gmail.com>
2024-09-30 16:08:47 +0300

    changed

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles.css';
-const CategoryList = ({ categories }) => {
-    return (
-        <div>
-            <h3>Categories</h3>
-            <ul>
-                {categories.map((category) => (
-                    <li key={category.id}>
-                        <Link to={`/categories/${category.slug}`}>{category.name}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+
+const CategoryList = ({ categories = [] }) => {
+  return (
+    <div>
+      <h3>Categories</h3>
+      <ul>
+        {categories.length > 0 ? (
+          categories.map((category) => (
+            <li key={category.id}>
+              <Link to={`/categories/${category.slug}`}>{category.name}</Link>
+            </li>
+          ))
+        ) : (
+          <li>No categories available</li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default CategoryList;
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCategories } from '../redux/action/categoriesActions';
 import { fetchProducts } from '../redux/action/productsActions';
-
 import CategoryList from '../components/CategoryList';
 import FlashSaleProducts from '../components/FlashSaleProducts';
 import TopSellingProducts from '../components/TopSellingProducts';
@@ -11,29 +9,35 @@ import CategoryProducts from '../components/CategoryProducts';
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const categories = useSelector((state) => state.categories);
-  const products = useSelector((state) => state.products);
+  
+  const categories = useSelector((state) => state.categories || []);
+  const products = useSelector((state) => state.products || []);
 
   useEffect(() => {
     dispatch(fetchCategories());
     dispatch(fetchProducts());
   }, [dispatch]);
+  
+  console.log("Products: ", products);  // Check if products are fetched
+  
+  // Filter flash sale products and top-selling products
+  const flashSaleProducts = products?.filter(product => product.is_flash_sale)?.slice(0, 5) || [];
 
-  const flashSaleProducts=products.filter(product => product.is_flash_sale).slice(0,5)
+  
   return (
     <>
       <div className="row">
         <div className="col-md-3">
-          <CategoryList categories={categories} />
+          {categories.length > 0 && <CategoryList categories={categories} />}
         </div>
         <div className="col-md-9">
-          <FlashSaleProducts products={flashSaleProducts} />
+          {flashSaleProducts.length > 0 && <FlashSaleProducts products={flashSaleProducts} />}
         </div>
       </div>
       <div className="row">
-        <TopSellingProducts products={products} />
+        {products.length > 0 && <TopSellingProducts products={products} />}
       </div>
-      {categories.map((category) => (
+      {products.length > 0 && categories.map((category) => (
         <div key={category.id} className="row">
           <CategoryProducts category={category} products={products} />
         </div>
