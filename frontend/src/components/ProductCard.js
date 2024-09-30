bf10889425793342c22db0c4b389fa2481bdf980
wokuma <wokumateshome13@gmail.com>
2024-09-30 16:10:08 +0300

    commit

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const ProductCard = ({product}) =>{
+  return (
+    <div className='card'>
+        <img src={product.image} className="card-image-top" alt={product.name} />
+        <div className='card-body'>
+            <h5 className="card-title" 
+            >{product.name}</h5>
+            <p className='card-text'>{product.description}</p>
+            <p>Price: ${product.price }</p>
+            <Link to={`/product/${product.slug}`} className='btn btn-primary'>View Detail</Link>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard 
---
+++ frontend/src/components/SearchResultsPage.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect} from "react";
+import { useLocation } from "react-router-dom";
+import ProductCard from "./ProductCard";
+
+const SearchResultsPage=()=>{
+
+    const location =useLocation()
+    const queryParams= new URLSearchParams(location.search)
+    const searchQuery=queryParams.get('q')
+    const [searchResults,setSearchResults]= useState([])
+
+    useEffect(()=>{
+        const fetchSearchResults =async()=>{
+
+            try{
+
+                const response = await fetch(`http://http://127.0.0.1:8000/api/products/q=${searchQuery}`)
+                if (!response.ok){
+                    throw new Error('failed to search result')
+                }
+
+                const data=await response.json()
+                setSearchResults(data)
+
+            }
+             catch(error){
+
+                console.error('Error fetching',error)
+
+             }
+        }
+        fetchSearchResults()
+    },[searchQuery])
+
+    return (
+        <div>
+            <h1>Search result for "{searchQuery}"</h1>
+            <div className="row">
+                {searchResults.map((product)=>
+                <div key={product.id} className="col-md-3">
+                    <ProductCard product={product}/>
+
+                </div>
+                )}
+            </div>
+        </div>
+    )
+}
+export default SearchResultsPage;
