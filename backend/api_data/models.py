2fdfa6bb3a8dd282cd98ee7ad8386f01e3e6624f
wokuma <wokumateshome13@gmail.com>
2024-10-04 10:13:06 +0300

    commit changed

--- backend/api_data/models.py
+++ backend/api_data/models.py
@@ -30,6 +30,7 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
+    
     vendor=models.ForeignKey(Vendor, on_delete=models.CASCADE,related_name='vendor_products')
     category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='category_products')
     name=models.CharField(max_length=100)
