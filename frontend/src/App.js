099bb7030770bfb72d4aeec9e34bf6228dcb87b9
wokuma <wokumateshome13@gmail.com>
2024-10-18 15:28:17 +0300

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
 import HomePage from './components/HomePage';
 import ProductDetails from './components/ProductDetails';
 import SearchResultsPage from './components/SearchResultsPage';
 import CategoryPage from './components/CategoryPage';
 import CartPage from './components/CartPage';
 import CheckoutPage from './components/CheckoutPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUser } from './action/authActions';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import VerifyEmail from './components/VerifyEmail';
+import ProtectedRoute from './components/ProtectedRoute';
+import PasswordResetRequest from './components/PasswordResetRequest';
+import PasswordResetConfirm from './components/PasswordResetConfirm';
+
 
 function SearchBar({ cart }) {
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
@@ -29,20 +39,21 @@ function SearchBar({ cart }) {
   };
 
   return (
-    <Form className="d-flex" role="search" onSubmit={handleSearchSubmit}>
-      <Form.Control
+    <form className="flex" role="search" onSubmit={handleSearchSubmit}>
+      <input
         type="search"
         placeholder="Search"
-        className="me-2"
+        className="p-2 rounded-xl border border-gray-300  h-10 w-96"
         aria-label="Search"
         value={searchQuery}
         onChange={handleSearchChange}
       />
-    </Form>
+    </form>
   );
 }
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const [cart, setCart] = useState(
     localStorage.getItem('cart') !== null ? JSON.parse(localStorage.getItem('cart')) : []
   );
@@ -50,42 +61,65 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand as={Link} to="/">MyShopping</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                <NavDropdown title="Category" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to="/categories/women-cloth">Women Cloth</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/categories/fruits">Fruits</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/categories/men-cloth">Men Cloth</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/categories/food">Food</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to="/account">My account</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link as={Link} to="/cart">Cart ({cart.length})</Nav.Link>
-              </Nav>
-              <SearchBar cart={cart} /> {/* Pass cart to SearchBar */}
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <nav className="bg-gray-800 p-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <Link to="/" className="text-white ml-24 text-lg font-bold">
+              MyShopping
+            </Link>
+            
+            <SearchBar cart={cart} />
+            <div className="relative group ">
+                <button className="text-white ml-64  ">Category</button>
+                <div className="absolute hidden group-hover:block bg-white text-gray-700  rounded-lg shadow-md">
+                  <Link to="/categories/engine" className="block p-2">Engine</Link>
+                  <Link to="/categories/transmission-system" className="block p-2">Transmission System</Link>
+                  <Link to="/categories/brake-system" className="block p-2">Brake System</Link>
+                  <Link to="/categories/steering-system" className="block p-2">Steering System</Link>
+                  {/* Add more categories as needed */}
+                </div>
+              </div>
+            <div className="space-x-4">
+              
+              <Link to="/cart" className="text-white ">
+                Cart ({cart.length})
+              </Link>
+              {isAuthenticated ? (
+                <>
+                  <Link to="/profile" className="text-white">Profile</Link>
+                  <Link to="/logout" className="text-white">Logout</Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/login" className="text-white">Login</Link>
+                  <Link to="/signup" className="text-white">Register</Link>
+                </>
+              )}
+            </div>
+          </div>
+        </nav>
 
-        <Container>
+        <main className="container mx-auto py-8">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="product/:slug" element={<ProductDetails />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
             <Route path="/search" element={<SearchResultsPage />} />
             <Route path="/cart" element={<CartPage cart={cart} setCart={setCart} />} />
-            <Route path="/checkout" element={<CheckoutPage cart={cart} setCart={setCart} />} />
+            <Route path="/checkout" element={<ProtectedRoute isAuthenticated={isAuthenticated}><CheckoutPage cart={cart} setCart={setCart} /></ProtectedRoute>} />
             <Route path="/categories/:slug" element={<CategoryPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/logout" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Logout /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile /></ProtectedRoute>} />
+            <Route path="/verify-email/:uid/:token" element={<VerifyEmail />} />
+            <Route path="/password-reset" element={<PasswordResetRequest />} />
+            <Route path="/reset-password/:uid/:token" element={<PasswordResetConfirm />} />
           </Routes>
-        </Container>
-      </Router>
+        </main>
 
-      <footer className="bg-success">© 2024 | Designed by Wokuma | All Rights Reserved</footer>
+        <footer className="bg-green-600 text-white text-center p-4">
+          © 2024 | Designed by Wokuma | All Rights Reserved
+        </footer>
+      </Router>
     </div>
   );
 }
