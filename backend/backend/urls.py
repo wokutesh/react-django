9c26f1805393de3f5422218fd5fc903201874d8c
wokuma <wokumateshome13@gmail.com>
2024-10-18 15:16:55 +0300

    commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('api_data.urls')),
+    path('accounts/', include('allauth.urls')),
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
